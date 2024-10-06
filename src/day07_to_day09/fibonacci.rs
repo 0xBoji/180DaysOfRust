fc05f659aabbee1717f87a5792d09b45a9722ec0
0xboji <hoangpichgoodkid@gmail.com>
2024-10-06 16:55:16 +0700

    fix: cannot infer type

--- src/day07_to_day09/fibonacci.rs
+++ src/day07_to_day09/fibonacci.rs
@@ -55,7 +55,7 @@ mod tests {
 
     #[test]
     fn test_fibonacci() {
-        assert_eq!(fibonacci(0), vec![]);
+        assert_eq!(fibonacci(0), Vec::<u32>::new());
         assert_eq!(fibonacci(1), vec![0]);
         assert_eq!(fibonacci(2), vec![0, 1]);
         assert_eq!(fibonacci(5), vec![0, 1, 1, 2, 3]);
